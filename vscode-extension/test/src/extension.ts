1c69aaa4505ea6c3a1e7f1a6888e08b290c5c412
Ian.lee <ian.lee@cupix.com>
2022-03-05 09:24:28 +0900

    word counter

--- vscode-extension/test/src/extension.ts
+++ vscode-extension/test/src/extension.ts
@@ -1,25 +1,90 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, window}  from 'vscode';
+
+class WordCounter {
+
+	private _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+
+	updateWordCount() {
+
+			// Get the current text editor
+			let editor = window.activeTextEditor;
+			if (!editor) {
+					this._statusBarItem.hide();
+					return;
+			}
+
+			let doc = editor.document;
+
+			if (doc.languageId === "markdown") {
+					let wordCount = this._getWordCount(doc);
+					// Update the status bar
+					this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+					this._statusBarItem.show();
+			} else {
+					this._statusBarItem.hide();
+			}
+	}
+
+	public _getWordCount(doc: TextDocument): number {
+
+			let docContent = doc.getText();
+
+			// Parse out unwanted whitespace so the split is accurate
+			docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+			docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+			let wordCount = 0;
+			if (docContent !== "") {
+					wordCount = docContent.split(" ").length;
+			}
+
+			return wordCount;
+	}
+
+	dispose() {
+			this._statusBarItem.dispose();
+	}
+}
+
+class WordCounterController {
+
+	private _wordCounter: WordCounter;
+	private _disposable: Disposable;
+
+	constructor(wordCounter: WordCounter) {
+			this._wordCounter = wordCounter;
+
+			// subscribe to selection change and editor activation events
+			let subscriptions: Disposable[] = [];
+			window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+			window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+
+			// update the counter for the current file
+			this._wordCounter.updateWordCount();
+
+			// create a combined disposable from both event subscriptions
+			this._disposable = Disposable.from(...subscriptions);
+	}
+
+	dispose() {
+			this._disposable.dispose();
+	}
+
+	private _onEvent() {
+			console.log('_onEvent');
+			this._wordCounter.updateWordCount();
+	}
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test" is now active!');
+export function activate(context: ExtensionContext) {
+	const worldCounter = new WordCounter();
+	const controller = new WordCounterController(worldCounter);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(worldCounter);
+	context.subscriptions.push(controller);
 }
 
 // this method is called when your extension is deactivated
